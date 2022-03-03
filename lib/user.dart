6efa095214c444625791a0425e4a462f4befcff9
curkirk <kirk.who.knew.eve.world@gmail.com>
2022-03-03 20:47:16 +0700

    kirk second deited

--- lib/user.dart
+++ lib/user.dart
@@ -1,21 +1,29 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
+class SelectUser extends StatefulWidget {
+  const SelectUser({Key? key}) : super(key: key);
+
+  @override
+  State<SelectUser> createState() => _SelectUserState();
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  // This widget is the root of your application.
+class _SelectUserState extends State<SelectUser> {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+    return Container(
+      child: GridView.count(
+        crossAxisCount: 2,
+        children: List.generate(5, (index) {
+          return Column(
+            children: [
+              Image.network(
+                  'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.behance.net%2Fgallery%2F33850498%2FNetflix-Profile-Avatars&psig=AOvVaw08ocOPSdn7EmVPjDUAoOvp&ust=1646401372842000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCID5uaKJqvYCFQAAAAAdAAAAABAD'),
+              Text('User name {$index}'),
+              const Icon( Icons.lock)
+            ],
+          );
+        }),
       ),
-      home:  Container(),
     );
   }
 }
