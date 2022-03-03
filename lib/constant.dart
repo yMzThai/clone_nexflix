2c89a3142085ed1baf6e88e62931c08631666fc0
yMz <ymz.thai@gmail.com>
2022-03-03 21:18:03 +0700

    master

--- lib/constant.dart
+++ lib/constant.dart
@@ -1,5 +1,17 @@
-class Images {
+import 'package:flutter/material.dart';
+
+class MyImages {
   static const String fullLogo = 'assets/images/NETFLIX_LOGO.png';
   static const String nLogo  = 'assets/images/Netflix_N_logo.png';
+}
 
+
+class MyStrings {
+  static const String whoSWatching = "Who's Wathing?";
+  static const String manageProfiles = "Manage Profiles";
+}
+
+
+class MyColors{
+  static const Color background = Color(0x00000000);
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:clone_nexflix/Widgets/appbarlock.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -11,11 +12,13 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home:  Container(),
+      home:  Column(mainAxisAlignment: MainAxisAlignment.start,
+      children:const[AppbarLock()]),
     );
   }
 }
