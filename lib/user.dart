bbf368c2e275404ba25c25199d593ed290eb6329
curkirk <kirk.who.knew.eve.world@gmail.com>
2022-03-03 23:31:28 +0700

    Kirk 5th edited

--- lib/user.dart
+++ lib/user.dart
@@ -1,40 +1,42 @@
 import 'package:flutter/material.dart';
 
-class SelectUser extends StatefulWidget {
-  const SelectUser({Key? key}) : super(key: key);
+class UserList extends StatefulWidget {
+  const UserList({Key? key}) : super(key: key);
 
   @override
-  State<SelectUser> createState() => _SelectUserState();
+  State<UserList> createState() => _UserListState();
 }
 
-class _SelectUserState extends State<SelectUser> {
+class _UserListState extends State<UserList> {
   @override
   Widget build(BuildContext context) {
-    return Container(
+    return ElevatedButton(
+      style: ElevatedButton.styleFrom(
+        primary: Colors.black
+      ),
+      onPressed: () {},
       child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
         children: [
-          const Text("Who's Watching"),
-          GridView.count(
-            crossAxisCount: 2,
-            children: List.generate(5, (index) {
-              return Column(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  ClipRRect(
-                      borderRadius: BorderRadius.circular(8),
-                      child: Image.network(
-                        'https://mir-s3-cdn-cf.behance.net/project_modules/disp/84c20033850498.56ba69ac290ea.png',
-                        width: 100,
-                      )),
-                  Text(
-                    'User name $index',
-                    style: const TextStyle(fontSize: 18),
-                  ),
-                  const Icon(Icons.lock)
-                ],
-              );
-            }),
+          ClipRRect(
+            borderRadius: BorderRadius.circular(8),
+            child: Image.network(
+              'https://mir-s3-cdn-cf.behance.net/project_modules/disp/84c20033850498.56ba69ac290ea.png',
+              width: 100,
+            ),
           ),
+          const Padding(
+            padding: EdgeInsets.symmetric(vertical: 5),
+            child: Text(
+              'KIRK',
+              style: TextStyle(fontSize: 12, color: Colors.white),
+            ),
+          ),
+          const Icon(
+            Icons.lock_outline,
+            color: Colors.grey,
+            size: 14,
+          )
         ],
       ),
     );
