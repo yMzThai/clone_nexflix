9685e2aa04dbb3d0c803d9841546cd7ee6402008
curkirk <kirk.who.knew.eve.world@gmail.com>
2022-03-03 21:16:53 +0700

    kirk 4th edited

--- lib/user.dart
+++ lib/user.dart
@@ -11,26 +11,32 @@ class _SelectUserState extends State<SelectUser> {
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: GridView.count(
-        crossAxisCount: 2,
-        children: List.generate(5, (index) {
-          return Column(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              ClipRRect(
-                  borderRadius: BorderRadius.circular(8),
-                  child: Image.network(
-                    'https://mir-s3-cdn-cf.behance.net/project_modules/disp/84c20033850498.56ba69ac290ea.png',
-                    width: 100,
-                  )),
-              Text(
-                'User name $index',
-                style: const TextStyle(fontSize: 18),
-              ),
-              const Icon(Icons.lock)
-            ],
-          );
-        }),
+      margin: const EdgeInsets.symmetric(vertical: 100 , horizontal: 40),
+      child: Column(
+        children: [
+          const Text("Who's Watching"),
+          GridView.count(
+            crossAxisCount: 2,
+            children: List.generate(5, (index) {
+              return Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  ClipRRect(
+                      borderRadius: BorderRadius.circular(8),
+                      child: Image.network(
+                        'https://mir-s3-cdn-cf.behance.net/project_modules/disp/84c20033850498.56ba69ac290ea.png',
+                        width: 100,
+                      )),
+                  Text(
+                    'User name $index',
+                    style: const TextStyle(fontSize: 18),
+                  ),
+                  const Icon(Icons.lock)
+                ],
+              );
+            }),
+          ),
+        ],
       ),
     );
   }
