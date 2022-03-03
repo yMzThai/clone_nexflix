4a94bb9395aa882cefa85f38a61c5a067ab01113
curkirk <kirk.who.knew.eve.world@gmail.com>
2022-03-03 21:07:37 +0700

    kirk 3th edited

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'user.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,11 +12,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home:  Container(),
+      home:  const SelectUser(),
     );
   }
 }
--- lib/user.dart
+++ lib/user.dart
@@ -15,11 +15,19 @@ class _SelectUserState extends State<SelectUser> {
         crossAxisCount: 2,
         children: List.generate(5, (index) {
           return Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: [
-              Image.network(
-                  'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.behance.net%2Fgallery%2F33850498%2FNetflix-Profile-Avatars&psig=AOvVaw08ocOPSdn7EmVPjDUAoOvp&ust=1646401372842000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCID5uaKJqvYCFQAAAAAdAAAAABAD'),
-              Text('User name {$index}'),
-              const Icon( Icons.lock)
+              ClipRRect(
+                  borderRadius: BorderRadius.circular(8),
+                  child: Image.network(
+                    'https://mir-s3-cdn-cf.behance.net/project_modules/disp/84c20033850498.56ba69ac290ea.png',
+                    width: 100,
+                  )),
+              Text(
+                'User name $index',
+                style: const TextStyle(fontSize: 18),
+              ),
+              const Icon(Icons.lock)
             ],
           );
         }),
