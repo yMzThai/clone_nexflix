52a34d8ac79cf47f9c088218710c77526d5260e0
yMz <ymz.thai@gmail.com>
2022-03-04 22:41:38 +0700

    master

---
+++ lib/userdetail.dart
@@ -0,0 +1,7 @@
+List<Map<String, Object?>> users = [
+{"Name":"Yean","Profile":"image","Password":123456},
+{"Name":"KIRK","Profile":"image","Password":null},
+{"Name":"N Pang","Profile":"image","Password":null},
+{"Name":"Mokun","Profile":"image","Password":null},
+{"Name":"Kids","Profile":"image","Password":null},
+];
