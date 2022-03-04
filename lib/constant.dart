00353d078e02a75736c45299fb5ec73e3312dd6d
curkirk <kirk.who.knew.eve.world@gmail.com>
2022-03-04 21:22:10 +0700

    master

--- lib/constant.dart
+++ lib/constant.dart
@@ -2,21 +2,20 @@ import 'package:flutter/material.dart';
 
 class MyImages {
   static const String fullLogo = 'assets/images/NETFLIX_LOGO.png';
-  static const String nLogo  = 'assets/images/Netflix_N_logo.png';
+  static const String nLogo = 'assets/images/Netflix_N_logo.png';
 }
-
 
 class MyStrings {
   static const String whoSWatching = "Who's Wathing?";
   static const String manageProfiles = "Manage Profiles";
 }
 
-
-class MyColors{
+class MyColors {
   static const Color background = Color(0xFF000000);
+  static const Color text = Color(0xFFFFFFFF);
 }
 
-class MyIcon{
+class MyIcon {
   static const IconData edit = Icons.create;
   static const IconData back = Icons.arrow_back;
 }
--- lib/user.dart
+++ lib/user.dart
@@ -1,3 +1,4 @@
+import 'package:clone_nexflix/constant.dart';
 import 'package:flutter/material.dart';
 
 class UserList extends StatefulWidget {
@@ -8,37 +9,45 @@ class UserList extends StatefulWidget {
 }
 
 class _UserListState extends State<UserList> {
+  bool locked = false;
+
+  Widget _profile() {
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(8),
+      child: Image.network(
+        'https://mir-s3-cdn-cf.behance.net/project_modules/disp/84c20033850498.56ba69ac290ea.png',
+        width: 100,
+      ),
+    );
+  }
+
+  Widget _textName() {
+    return const Padding(
+      padding: EdgeInsets.symmetric(vertical: 5),
+      child: Text(
+        'KIRK',
+        style: TextStyle(fontSize: 12, color: MyColors.text),
+      ),
+    );
+  }
+
+  Widget iconLock() {
+    return const Icon(
+      Icons.lock_outline,
+      color: Colors.grey,
+      size: 14,
+    );
+  }
+
+  Widget unLock() {
+    return const SizedBox(height: 14.0);
+  }
+
   @override
   Widget build(BuildContext context) {
-    return ElevatedButton(
-      style: ElevatedButton.styleFrom(
-        primary: Colors.black
-      ),
-      onPressed: () {},
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          ClipRRect(
-            borderRadius: BorderRadius.circular(8),
-            child: Image.network(
-              'https://mir-s3-cdn-cf.behance.net/project_modules/disp/84c20033850498.56ba69ac290ea.png',
-              width: 100,
-            ),
-          ),
-          const Padding(
-            padding: EdgeInsets.symmetric(vertical: 5),
-            child: Text(
-              'KIRK',
-              style: TextStyle(fontSize: 12, color: Colors.white),
-            ),
-          ),
-          const Icon(
-            Icons.lock_outline,
-            color: Colors.grey,
-            size: 14,
-          )
-        ],
-      ),
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [_profile(), _textName(), locked ? iconLock() : unLock()],
     );
   }
 }
